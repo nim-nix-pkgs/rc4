694aa51435760791cf4067d764f76ed54b16a11d
hugosenari <hugosenari@gmail.com>
2022-03-12 00:12:13 -0300

    chore: re index rc4

---
+++ master/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''RC4 library implementation'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-RC4-master.flake = false;
+  inputs.src-RC4-master.owner = "OHermesJunior";
+  inputs.src-RC4-master.ref   = "refs/heads/master";
+  inputs.src-RC4-master.repo  = "nimRC4";
+  inputs.src-RC4-master.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-RC4-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-RC4-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
